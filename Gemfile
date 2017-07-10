849a3d666dd86554dd315d7f89efba5c0755af4a
islusar <islusar@nravo.com>
2017-07-10 12:43:12 +0300

    Add rubocop gem.

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'jbuilder', '~> 2.5'
 
 #custom gems
 gem 'devise'
+gem 'rubocop', require: false
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
 10.times do |i|
-  User.create(email: "test#{i}@test.com", password: "123123", password_confirmation: "123123", first_name: "Tester_#{i}", last_name: "Test")
+  User.create(email: "test#{i}@test.com",
+              password: '123123',
+              password_confirmation: '123123',
+              first_name: "Tester_#{i}",
+              last_name: 'Test')
 end
-AdminUser.create(email: "admin@admin.com", password: "123123", password_confirmation: "123123", first_name: "Admin", last_name: "User")
+AdminUser.create(email: 'admin@admin.com',
+                 password: '123123',
+                 password_confirmation: '123123',
+                 first_name: 'Admin',
+                 last_name: 'User')
